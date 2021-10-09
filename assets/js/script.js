9631a94d1b65736d9e060321d3afaa6333c107e5
Otacilio Lima <otacilio.dml@hotmail.com>
2021-10-09 16:55:44 -0300

    kenzietter

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,106 +1,9 @@
-// VARIÁVEIS
-// uma caixinha na memória, onde eu guardo algum valor
-
-// var => má prática, não utilizem
-
-// let
-// const
-
-let numero = 0;
-const nome = "Maria";
-const boleano = false;
-
-// console.log(numero);
-// console.log(nome);
-// console.log(boleano);
-
-/*
-operadores básicos
-
-soma: +
-subtração: -
-multiplicação: *
-divisão: /
-*/
-
-let numero2 = 3;
-let numero3 = 5;
-
-let soma = 2 + 2;
-let soma2 = numero2 + numero3;
-
-// console.log(soma)
-// console.log(soma2)
-
-/**
- * operadores lógicos
- * 
- * == / ===
- * != / !==
- * > / >=
- * < / <=
- * 
- */
-
-// console.log(!(numero2 > numero3));
-
-/**
- * funções
- */
-
-// declaração apenas
-function calcularSoma(numero1, numero2) {
-    let total = numero1 + numero2;
-    console.log(total);
-}
-
-// chamada da função -> momento em que ela vai ser exacutada
-// calcularSoma(7, 9);
-// calcularSoma(2, 5);
-// calcularSoma(7, 4);
-// calcularSoma(1, 9);
-
-/**
- * 
- * DOM
- * Document Object Model(Modelo de Objeto do Documento)
- * 
- */
-
-const cabecalho = document.querySelector('header');
-
-// console.log(cabecalho);
-
-/**
- * 
- * eventos
- * 
- * addEventListener
- * 
- * elemento.addEventListener('evento', função);
- * 
- */
-
-// const tweetar = document.querySelector('button');
-
-// function imprimirNoConsole() {
-//     console.log('clicou no botão!')
-// }
-
-// tweetar.addEventListener('click', imprimirNoConsole);
-
-/**
- * passo a passo
- * 
- * 1º passo: capturar o valor da textarea para criar o tweet
- * 2º passo: construir o tweet
- * 3º passo: imprimir esse tweet
- * 
- */
 
 const textarea = document.querySelector('textarea');
 const tweetar = document.querySelector('button');
-const feed = document.querySelector(".conteudoPrincipal__listaTweets")
+const feed = document.querySelector(".conteudoPrincipal__listaTweets");
+const btnDarkMode = document.querySelector('.btn-dark');
+const tela = document.querySelector('body');
 
 function pegarTweet(event) {
     event.preventDefault();
@@ -112,7 +15,6 @@ function pegarTweet(event) {
 tweetar.addEventListener('click', pegarTweet);
 
 
-
 function criarTweet(tweetTexto){
     
     let data = new Date();
@@ -121,9 +23,9 @@ function criarTweet(tweetTexto){
 
     //OBJETO
     const tweet = {
-        nome: "Daniel",
-        foto: "./assets/img/ProfilePic.png",
-        usuario: "@danielkenzie",
+        nome: "Thresh",
+        foto: "./assets/img/ProfilePic.jpg",
+        usuario: "@thresh.porito",
         texto:tweetTexto,
         tempo:`${hora}:${minutos}`
     }
@@ -167,4 +69,8 @@ function listarTemplateTweet(tweet){
     textarea.value = ""
 }   
 
+btnDarkMode.onclick = darkMode;
 
+function darkMode() {
+    tela.classList.toggle('dark');
+}
